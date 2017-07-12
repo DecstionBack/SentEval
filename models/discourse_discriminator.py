044bac97de212d280bc58e3948b2dcddd344e5aa
windweller <leo.niecn@gmail.com>
2017-07-12 10:43:05 -0700

    start to add Tensorflow models into the evaluation pipeline

---
+++ models/discourse_discriminator.py
@@ -0,0 +1,9 @@
+"""
+Using discourse discrimination
+to produce sentence embeddings
+"""
+
+# Plan:
+# move the model here (not training here)
+# and have the model here load in train_dir from elsewhere
+# eventually this whole thing can just run on the cloud (or locally)

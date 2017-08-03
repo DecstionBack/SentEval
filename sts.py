3b33680f2cddfae71956f4f97b9fc5ae0490c789
windweller <leo.niecn@gmail.com>
2017-08-03 10:16:29 -0700

    fix sts error

--- sts.py
+++ sts.py
@@ -43,8 +43,8 @@ class STSEval(object):
         
 
     def do_prepare(self, params, prepare):
-        if similarity in params:
-            self.similarity = similarity
+        if "similarity" in params:
+            self.similarity = params['similarity']
         else: # Default similarity is cosine
             self.similarity = lambda s1,s2: cosine(np.nan_to_num(s1), np.nan_to_num(s2))
         return prepare(params, self.samples)

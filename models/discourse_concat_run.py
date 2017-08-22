84f6a34718de496a7c7d70c4c9949dcd68b27b18
windweller <leo.niecn@gmail.com>
2017-08-22 15:53:23 -0700

    add batch_size option and by lowering batch size we can have both PyTorch and TF model available

--- models/discourse_concat_run.py
+++ models/discourse_concat_run.py
@@ -21,7 +21,7 @@ tf.app.flags.DEFINE_integer("max_seq_len", 35, "number of time steps to unroll f
 tf.app.flags.DEFINE_integer("embed_size", 300, "dimension of GloVE vector to use")
 tf.app.flags.DEFINE_integer("learning_rate_decay_epoch", 1, "Learning rate starts decaying after this epoch.")
 tf.app.flags.DEFINE_float("dropout", 0., "probability of dropping units")
-tf.app.flags.DEFINE_integer("batch_size", 100, "batch size")
+tf.app.flags.DEFINE_integer("batch_size", 128, "batch size, but for higher dimension vectors, reduce to 64")
 tf.app.flags.DEFINE_integer("seed", 123, "random seed to use")
 tf.app.flags.DEFINE_float("init_scale", 0.1, "scale for random initialization")
 tf.app.flags.DEFINE_float("learning_rate", 0.01, "initial learning rate")
@@ -144,6 +144,7 @@ def main(_):
 
         params_senteval.discourse = discourse_sc
         params_senteval.snli = snli_sc
+        params_senteval.batch_size = FLAGS.batch_size
 
         # restore the model here
         # (two models are stored together)

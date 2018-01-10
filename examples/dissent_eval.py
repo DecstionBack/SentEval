aa9d4df6d3af807b4183383b31afa919b2061bdf
windweller <leo.niecn@gmail.com>
2018-01-09 19:51:38 -0800

    fix small mistake

--- examples/dissent_eval.py
+++ examples/dissent_eval.py
@@ -132,7 +132,7 @@ Evaluation of trained model on Transfer Tasks (SentEval)
 
 # define transfer tasks
 transfer_tasks = ['MR', 'CR', 'SUBJ', 'MPQA', 'SST', 'TREC', 'SICKRelatedness',
-                  'SICKEntailment', 'MRPC', 'STS14'] if not params.dis_snli else ['SNLI', 'DIS']
+                  'SICKEntailment', 'MRPC', 'STS14'] if not params.snli_dis else ['SNLI', 'DIS']
 
 # define senteval params
 params_senteval = dotdict({'usepytorch': True, 'task_path': PATH_TO_DATA,

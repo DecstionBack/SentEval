249c4ee03c77dcfed7ae1bc4823a70dc4a80bd06
windweller <leo.niecn@gmail.com>
2018-07-05 17:27:08 -0700

    update scripts to accommondate PyTorch 0.4

--- senteval/tools/classifier.py
+++ senteval/tools/classifier.py
@@ -1,29 +1,18 @@
-# Copyright (c) 2017-present, Facebook, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-#
-
-"""
-Pytorch Classifier class in the style of scikit-learn
-Classifiers include Logistic Regression and MLP
-"""
-
 from __future__ import absolute_import, division, unicode_literals
 
 import numpy as np
 import copy
+from senteval import utils
 
 import torch
 from torch import nn
 from torch.autograd import Variable
-import torch.optim as optim
+import torch.nn.functional as F
 
 
 class PyTorchClassifier(object):
     def __init__(self, inputdim, nclasses, l2reg=0., batch_size=64, seed=1111,
-                 cudaEfficient=False, nepoches=4, maxepoch=200):
+                 cudaEfficient=False):
         # fix seed
         np.random.seed(seed)
         torch.manual_seed(seed)
@@ -34,8 +23,6 @@ class PyTorchClassifier(object):
         self.l2reg = l2reg
         self.batch_size = batch_size
         self.cudaEfficient = cudaEfficient
-        self.nepoches = nepoches
-        self.maxepoch = maxepoch
 
     def prepare_split(self, X, y, validation_data=None, validation_split=None):
         # Preparing validation data
@@ -45,8 +32,8 @@ class PyTorchClassifier(object):
             devX, devy = validation_data
         else:
             permutation = np.random.permutation(len(X))
-            trainidx = permutation[int(validation_split * len(X)):]
-            devidx = permutation[0:int(validation_split * len(X))]
+            trainidx = permutation[int(validation_split*len(X)):]
+            devidx = permutation[0:int(validation_split*len(X))]
             trainX, trainy = X[trainidx], y[trainidx]
             devX, devy = X[devidx], y[devidx]
 
@@ -75,22 +62,22 @@ class PyTorchClassifier(object):
                                                         validation_split)
 
         # Training
-        while not stop_train and self.nepoch <= self.maxepoch:
-            self.trainepoch(trainX, trainy, nepoches=self.nepoches)
+        while not stop_train and self.nepoch <= self.max_epoch:
+            self.trainepoch(trainX, trainy, epoch_size=self.epoch_size)
             accuracy = self.score(devX, devy)
             if accuracy > bestaccuracy:
                 bestaccuracy = accuracy
                 bestmodel = copy.deepcopy(self.model)
             elif early_stop:
-                if early_stop_count >= 5:
+                if early_stop_count >= self.tenacity:
                     stop_train = True
                 early_stop_count += 1
         self.model = bestmodel
         return bestaccuracy
 
-    def trainepoch(self, X, y, nepoches=1):
+    def trainepoch(self, X, y, epoch_size=1):
         self.model.train()
-        for _ in range(self.nepoch, self.nepoch + nepoches):
+        for _ in range(self.nepoch, self.nepoch + epoch_size):
             permutation = np.random.permutation(len(X))
             all_costs = []
             for i in range(0, len(X), self.batch_size):
@@ -103,22 +90,16 @@ class PyTorchClassifier(object):
                 if self.cudaEfficient:
                     Xbatch = Xbatch.cuda()
                     ybatch = ybatch.cuda()
-
-                if not self.bilinear:
-                    output = self.model(Xbatch)
-                else:
-                    assert self.inputdim % 2 == 0  # hid dim must be even (this is feature dimension)
-                    Xbatch_vec1, Xbatch_vec2 = torch.split(Xbatch, split_size=self.inputdim/2, dim=1)
-                    output = self.model(Xbatch_vec1, Xbatch_vec2)
+                output = self.model(Xbatch)
                 # loss
                 loss = self.loss_fn(output, ybatch)
-                all_costs.append(loss.data[0])
+                all_costs.append(loss.data.item())
                 # backward
                 self.optimizer.zero_grad()
                 loss.backward()
                 # Update parameters
                 self.optimizer.step()
-        self.nepoch += nepoches
+        self.nepoch += epoch_size
 
     def score(self, devX, devy):
         self.model.eval()
@@ -134,8 +115,8 @@ class PyTorchClassifier(object):
                 ybatch = ybatch.cuda()
             output = self.model(Xbatch)
             pred = output.data.max(1)[1]
-            correct += pred.long().eq(ybatch.data.long()).sum()
-        accuracy = 1.0 * correct / len(devX)
+            correct += pred.long().eq(ybatch.data.long()).sum().item()
+        accuracy = 1.0*correct / len(devX)
         return accuracy
 
     def predict(self, devX):
@@ -156,117 +137,56 @@ class PyTorchClassifier(object):
         probas = []
         for i in range(0, len(devX), self.batch_size):
             Xbatch = Variable(devX[i:i + self.batch_size], volatile=True)
+            vals = F.softmax(self.model(Xbatch).data.cpu().numpy())
             if not probas:
-                probas = self.model(Xbatch).data.cpu().numpy()
+                probas = vals
             else:
-                probas = np.concatenate(probas,
-                                        self.model(Xbatch).data.cpu().numpy(),
-                                        axis=0)
+                probas = np.concatenate(probas, vals, axis=0)
         return probas
 
 
 """
-Logistic Regression with Pytorch
+MLP with Pytorch (nhid=0 --> Logistic Regression)
 """
-
-
-class LogReg(PyTorchClassifier):
-    def __init__(self, inputdim, nclasses, l2reg=0., batch_size=64,
-                 seed=1111, cudaEfficient=False, bilinear=False):
-        super(self.__class__, self).__init__(inputdim, nclasses, l2reg,
-                                             batch_size, seed, cudaEfficient)
-        self.bilinear = bilinear
-        if bilinear:
-            self.model = nn.Sequential(
-                nn.Bilinear(self.inputdim / 2, self.inputdim, self.inputdim),  # no dimension reduction at all
-                nn.Linear(self.inputdim, self.nclasses),
-            ).cuda()
-        else:
-            self.model = nn.Sequential(
-                nn.Linear(self.inputdim, self.nclasses),
-            ).cuda()
-        self.loss_fn = nn.CrossEntropyLoss().cuda()
-        self.loss_fn.size_average = False
-        self.optimizer = optim.Adam(self.model.parameters(),
-                                    weight_decay=self.l2reg)
-
-
-"""
-MLP with Pytorch
-"""
-
 
 class MLP(PyTorchClassifier):
-    def __init__(self, inputdim, hiddendim, nclasses, l2reg=0., batch_size=64,
-                 seed=1111, cudaEfficient=False, bilinear=False):
-        super(self.__class__, self).__init__(inputdim, nclasses, l2reg,
-                                             batch_size, seed, cudaEfficient)
-
-        self.hiddendim = hiddendim
-        self.bilinear = bilinear
-
-        if bilinear:
-            self.model = nn.Sequential(
-                nn.Bilinear(self.inputdim / 2, self.inputdim, self.inputdim),  # no dimension reduction at all
-                nn.Linear(self.inputdim, self.hiddendim),
-                # nn.BatchNorm1d(self.hiddendim),
-                nn.PReLU(),
-                nn.Linear(self.hiddendim, self.hiddendim),
-                # nn.BatchNorm1d(self.hiddendim),
-                nn.PReLU(),
-                nn.Linear(self.hiddendim, self.nclasses),
-            ).cuda()
-        else:
-            self.model = nn.Sequential(
-                nn.Linear(self.inputdim, self.hiddendim),
-                # nn.BatchNorm1d(self.hiddendim),
-                nn.PReLU(),
-                # nn.Linear(self.hiddendim, self.hiddendim),
-                # nn.BatchNorm1d(self.hiddendim),
-                # nn.PReLU(),
-                nn.Linear(self.hiddendim, self.nclasses),
-            ).cuda()
-
-        self.loss_fn = nn.CrossEntropyLoss().cuda()
-        self.loss_fn.size_average = False
-        self.optimizer = optim.Adam(self.model.parameters(),
-                                    weight_decay=self.l2reg)
-
-class FCNet(PyTorchClassifier):
-    def __init__(self, inputdim, hiddendim, nclasses, l2reg=0., batch_size=64,
+    def __init__(self, params, inputdim, nclasses, l2reg=0., batch_size=64,
                  seed=1111, cudaEfficient=False):
         super(self.__class__, self).__init__(inputdim, nclasses, l2reg,
                                              batch_size, seed, cudaEfficient)
+        """
+        PARAMETERS:
+        -nhid:       number of hidden units (0: Logistic Regression)
+        -optim:      optimizer ("sgd,lr=0.1", "adam", "rmsprop" ..)
+        -tenacity:   how many times dev acc does not increase before stopping
+        -epoch_size: each epoch corresponds to epoch_size pass on the train set
+        -max_epoch:  max number of epoches
+        -dropout:    dropout for MLP
+        """
 
-        self.hiddendim = hiddendim
+        self.nhid = 0 if "nhid" not in params else params["nhid"]
+        self.optim = "adam" if "optim" not in params else params["optim"]
+        self.tenacity = 5 if "tenacity" not in params else params["tenacity"]
+        self.epoch_size = 4 if "epoch_size" not in params else params["epoch_size"]
+        self.max_epoch = 200 if "max_epoch" not in params else params["max_epoch"]
+        self.dropout = 0. if "dropout" not in params else params["dropout"]
+        self.batch_size = 64 if "batch_size" not in params else params["batch_size"]
 
-        self.model = nn.Sequential(
-            nn.Linear(self.inputdim, self.hiddendim),
-            nn.Linear(self.hiddendim, self.hiddendim),
-            nn.Linear(self.hiddendim, self.nclasses)
-        ).cuda()
+        if params["nhid"] == 0:
+            self.model = nn.Sequential(
+                nn.Linear(self.inputdim, self.nclasses),
+                ).cuda()
+        else:
+            self.model = nn.Sequential(
+                nn.Linear(self.inputdim, params["nhid"]),
+                nn.Dropout(p=self.dropout),
+                nn.Sigmoid(),
+                nn.Linear(params["nhid"], self.nclasses),
+                ).cuda()
 
         self.loss_fn = nn.CrossEntropyLoss().cuda()
         self.loss_fn.size_average = False
 
-"""
-Build K-means classifier here
-
-Note: this is NOT KNN, the goal of this classifier
-is to simulate a clustering scenario.
-"""
-class KMeansClassifier(object):
-    def __init__(self, inputdim, nclasses, l2reg=0., batch_size=64, seed=1111,
-                 cudaEfficient=False, nepoches=4, maxepoch=200):
-        # fix seed
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
-
-        self.inputdim = inputdim
-        self.nclasses = nclasses
-        self.l2reg = l2reg
-        self.batch_size = batch_size
-        self.cudaEfficient = cudaEfficient
-        self.nepoches = nepoches
-        self.maxepoch = maxepoch
+        optim_fn, optim_params = utils.get_optimizer(self.optim)
+        self.optimizer = optim_fn(self.model.parameters(), **optim_params)
+        self.optimizer.param_groups[0]['weight_decay'] = self.l2reg
